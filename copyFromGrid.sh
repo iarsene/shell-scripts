aa6d7b948250a2680600bbc7f020dc8e966e3e20
Ionut Arsene <Ionut.Cristian.Arsene@cern.ch>
2017-04-03 10:46:58 +0200

    changed handling of 000 in run numbers for the copyFromGrid script; changed permissions to the scripts

--- copyFromGrid.sh
+++ copyFromGrid.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+alienHomeDir=/alice/cern.ch/user/i/iarsene/
+inputDir=$1
+outDir=$2
 runList=`cat $3`
 
 for run in $runList; do
-   mkdir -p $2/000$run
-   alien_ls /alice/cern.ch/user/i/iarsene/$1/000$run/ > $2/000$run/chunkList.txt
-   nchunks=`cat $2/000$run/chunkList.txt | wc -l`
+   mkdir -p $outDir/$run
+   alien_ls $alienHomeDir/$inputDir/$run/ > $outDir/$run/chunkList.txt
+   nchunks=`cat $outDir/$run/chunkList.txt | wc -l`
    echo "Copying $nchunks files for run $run from grid"
-   chunkList=`cat $2/000$run/chunkList.txt`
+   chunkList=`cat $outDir/$run/chunkList.txt`
    for chunk in $chunkList; do
-      mkdir -p $2/000$run/$chunk
-      alien_cp alien:///alice/cern.ch/user/i/iarsene/$1/000$run/$chunk/dstTree.root $2/000$run/$chunk/
+      mkdir -p $outDir/$run/$chunk
+      alien_cp alien://$alienHomeDir/$inputDir/$run/$chunk/dstTree.root $outDir/$run/$chunk/
    done
 done
    
